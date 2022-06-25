387f032d6bb5d60682103e9be326a52948551a33
munarh <102286286+munarh@users.noreply.github.com>
2022-06-25 08:41:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let firstName = "Munachimso"
+let lastName = "Irozuru"
+let height = "1.78m"
+let country = "Nigeria"
+
+let fullName = firstName + ' ' + lastName + "\n";
+
+document.write('Name: ' + fullName + "<br>");
+document.write('Height: ' + height + "<br>");
+document.write('Country: ' + country + "<br>");
+
+
+alert("My name is " + fullName + "\nMy height is " + height + "\nMy country is " + country)
